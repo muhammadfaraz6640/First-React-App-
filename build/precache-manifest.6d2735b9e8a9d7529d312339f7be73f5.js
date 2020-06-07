11422ace0a1623476d6d6b25ae097be2c55a8fa7
Muhammad Faraz Ansar <49414010+muhammadfaraz6640@users.noreply.github.com>
2020-06-07 21:54:01 +0500

    First React App

---
+++ build/precache-manifest.6d2735b9e8a9d7529d312339f7be73f5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1882344c01f17e5dcdde8b219ea16f99",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c00529e41c430390e480",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "eb167a132135f2e98285",
+    "url": "/static/js/2.ebe29839.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ebe29839.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c00529e41c430390e480",
+    "url": "/static/js/main.20fc60cb.chunk.js"
+  },
+  {
+    "revision": "c0832e453b97a1c23294",
+    "url": "/static/js/runtime-main.4eba2ab0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
